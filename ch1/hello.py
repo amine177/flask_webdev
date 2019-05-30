b07269cd3523c5370280c062d0ca08c66e206165
aminos <amri.amine@protonmail.com>
2019-05-30 19:11:31 +0100

    parsing header infos from http request

--- ch1/hello.py
+++ ch1/hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>'
+    user_agent = request.headers.get('User-agent')
+    return '<h1>Hello world!</h1><p>You are using {}</p>'.format(user_agent)
 
 @app.route('/user/<name>')
 def user(name):
